ea6beae4886e905e0791ecbb1d93a85aa68a3199
NachoVilallave <nvilallave@gmail.com>
2024-11-29 09:30:02 +0100

    streamlit

--- streamlit/weather_app.py
+++ streamlit/weather_app.py
@@ -19,48 +19,48 @@ def get_weather_forecast(lat, lon):
         return None
 
 # STEP4: Aplicacion streamlit
-st.title("Weather Forecast App")
-st.write("Get the weather forecast for any location using Open-Meteo API.")
+st.title("Aplicación predicción meteorológica")
+st.write("Obtenga el pronóstico del tiempo para cualquier ubicación utilizando la API Open-Meteo.")
 st.write("https://open-meteo.com/en/docs")
-st.write("https://streamlit.io/: To create interactive web apps")
+st.write("https://streamlit.io/: Para crear webs interactivas")
 
 # STEP5: Organizamos las entradas y el botón en columnas para evitar desplazamiento vertical
 # Ej.: Castellon --> lat = 39.98567, long=-0.04935
 col1, col2 = st.columns([1, 1])
 with col1:
-    lat = st.number_input("Enter Latitude:", value=0.0, format="%.6f", step=0.000001)
+    lat = st.number_input("Itroducir Latitud:", value=0.0, format="%.6f", step=0.000001)
 with col2:
-    lon = st.number_input("Enter Longitude:", value=0.0, format="%.6f", step=0.000001)
+    lon = st.number_input("Introducir Longitud:", value=0.0, format="%.6f", step=0.000001)
 
-fetch_button = st.button("Get Forecast")
+fetch_button = st.button("Obtener Predicción")
 
 
 # STEP6: Validamos lo que introduce el usuario
 if lat < -90 or lat > 90:
-    st.error("Latitude must be between -90 and 90.")
+    st.error("Latitud debe estar entre -90 y 90.")
 if lon < -180 or lon > 180:
-    st.error("Longitude must be between -180 and 180.")
+    st.error("Longitud debe estar entre -180 y 180.")
 
 # STEP7: Busca la previsión meteorológica
 if fetch_button and (-90 <= lat <= 90) and (-180 <= lon <= 180):
-    with st.spinner("Fetching data..."):
+    with st.spinner("Obteniendo datos..."):
         data = get_weather_forecast(lat, lon)
     if data:
         hourly_data = data['hourly']['temperature_2m']
         times = pd.to_datetime(data['hourly']['time'])
 
-        st.write(f"Weather forecast for Latitude: {lat}, Longitude: {lon}")
+        st.write(f"Pronóstico del tiempo para Latitud: {lat}, Longitud: {lon}")
 
         # Crea un DataFrame para visualización
-        forecast_data = {'Time': times, 'Temperature (°C)': hourly_data}
+        forecast_data = {'Tiempo': times, 'Temperatura (°C)': hourly_data}
         forecast_df = pd.DataFrame(forecast_data)
 
         # Muestra los datos en un gráfico de líneas usando la libreria Plotly
-        fig = px.line(forecast_df, x='Time', y='Temperature (°C)', title='Temperature Forecast')
-        fig.update_layout(xaxis_title='Time', yaxis_title='Temperature (°C)')
+        fig = px.line(forecast_df, x='Tiempo', y='Temperatura (°C)', title='Predicción temperatura')
+        fig.update_layout(xaxis_title='Tiempo', yaxis_title='Temperatura (°C)')
         st.plotly_chart(fig)
 
 # STEP8: Ejecuta la App
 #Para que el programa encuentre la libreria streamlit:
-#export PATH="/home/usuario/.local/bin/:$PATH" <-- abajo en el recuadro del terminal
+#export PATH="/home/nacvilvil/.local/bin/:$PATH" <-- abajo en el recuadro del terminal
 #streamlit run weather_app.py <-- abajo en el recuadro del terminal
